722ac8a41c352a175c722d9d7e5804852172ec62
yhnoh <tedgom19@gmail.com>
2021-07-25 17:42:02 +0900

    first commit

---
+++ src/test/java/com/querydsl/inflearn/InflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.querydsl.inflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
